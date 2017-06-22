ec69e468c9096d1a60648f6ccb8468ebbed5a738
Mentatt <greg.benoit@mentatt.com>
2017-06-22 12:03:56 -0500

    Added assignment functions
    Added the requested functions from Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions compute and store the inverse of an invertable matrix,
+## and then check to see if the inverse of future passed matrices can be retrieved
+## from cache or not.
 
-## Write a short comment describing this function
+## The following function computes the inverse of a matrix and stores it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    y <<- x   # Store the passed matrix in cache for later comparison
+    inv <<- solve(x)   # Store the inverse of the passed matrix
+    inv
 }
 
 
-## Write a short comment describing this function
+## The following function checks to see if the passed matrix's inverse
+## is already stored in cache and, if so, returns the inverse from cache.
+## If not, it calls the makeCacheMatrix function to get the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (identical(x,y)) {  # Check if the passed matrix is already stored in cache
+        ret_inv <- inv   #If so, return inverse from cached value
+    } else {
+        ret_inv <- makeCacheMatrix(x)  # Otherwise, compute the inverse again
+    }
+    ret_inv
 }
